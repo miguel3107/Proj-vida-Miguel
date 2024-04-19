dd7d9a7f6ffd12092f2eefd5cf7a20cafeb34c88
miguel3107 <108474194+miguel3107@users.noreply.github.com>
2024-04-19 10:44:22 -0300

    Add files via upload
    17/04

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ for (let i=0; i<botoes.length; i++) {
         textos[i].classList.add("ativo")
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo1 = new Date("2024-07-05T00:00:00");
+
+
+contadores[0].textContent = cauculaTempo(tempoObjetivo1);
+
+function cauculaTempo (tempoObjetivo){
+    const tempoAtual = Date();
+    let tempofinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempofinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+}
